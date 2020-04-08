c288d374a3f2caa106bb3826b7d33ecb4bb559c8
jamieHH <jamiehh@live.com>
2020-04-08 21:39:34 +0100

    Generating mipmaps for 2d textures

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -9,10 +9,7 @@ import java.util.List;
 import models.RawModel;
 
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
@@ -37,6 +34,10 @@ public class Loader {
 		try {
 			texture = TextureLoader.getTexture("PNG",
 					new FileInputStream("res/" + fileName + ".png"));
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println("Tried to load texture " + fileName + ".png , didn't work");
