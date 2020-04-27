3f3825f1ec4d78027e1d65152bdd4da8dacff0ed
jamieHH <jamiehh@live.com>
2020-04-28 00:01:31 +0100

    Adding this line

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -1,11 +1,7 @@
 package renderEngine;
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.opengl.*;
 
 public class DisplayManager {
 	
@@ -23,6 +19,7 @@ public class DisplayManager {
 		try {
 			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
 			Display.create(new PixelFormat().withSamples(16), attribs);
+			GL11.glEnable(GL13.GL_MULTISAMPLE);
 			Display.setTitle("LWJGL Engine");
 		} catch (LWJGLException e) {
 			e.printStackTrace();
